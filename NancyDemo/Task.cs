cd811d62001f55d47a735a84b505979ac6915911
edtyl3r <edtyler42@gmail.com>
2015-09-24 22:12:05 +0100

    first commit

---
+++ NancyDemo/Task.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace NancyDemo
+{
+    public class Task
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ NancyDemo/TaskModule.cs
@@ -0,0 +1,41 @@
+﻿using Nancy;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Nancy.ModelBinding;
+
+namespace NancyDemo
+{
+    public class TaskModule: NancyModule
+    {
+        private readonly IMongoCollection<Task> _tasks;
+
+        public TaskModule(IMongoCollection<Task> tasks)
+            : base("/api") 
+        {
+            _tasks = tasks;
+
+            Get["/tasks"] = GetTask;
+            Post["/tasks"] = AddTask;
+            
+        }
+
+        private Response GetTask(dynamic parameters)
+        {
+            return Response.AsJson(_tasks.Find("{}").ToListAsync().Result.ToJson());
+        }
+
+        private Response AddTask(dynamic parameters)
+        {
+            var model = this.Bind<Task>();
+
+            if (model.Text == string.Empty)
+            {
+                return HttpStatusCode.BadRequest;
+            }
+
+            _tasks.InsertOneAsync(model);
+
+            return HttpStatusCode.OK;
+        }
+    }
+}
